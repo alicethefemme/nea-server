058770f9fc69869770ac3b745aeb58aaa5f9e7cb
alicethefemme <alicethefemme@atfdev.co.uk>
2025-04-25 11:50:56 +0100

    Made docstrings for the methods.

--- common/helpers/database.js
+++ common/helpers/database.js
@@ -6,7 +6,6 @@ const bcrypt = require('bcrypt');
 module.exports = class MyDatabase {
     db;
 
-
     constructor() {
         const thisConfig = new Config();
         this.db = new Databasee(thisConfig.databasePath);
@@ -21,23 +20,44 @@ module.exports = class MyDatabase {
             ');').run();
     }
 
+    /**
+     * Gets the user ID from the database, provided the username is valid.
+     * @param username {String} The string username to look up in the database.
+     * @returns {int|null} The ID for the user.
+     */
     get_user(username) {
-        const statement = this.db.prepare('SELECT * FROM users WHERE username = ?');
-        return statement.get(username);
+        const statement = this.db.prepare('SELECT id FROM users WHERE username = ?');
+        let val =  statement.get(username);
+        if(!val) return null;
+        return val.id;
     }
 
+    /**
+     * Gets the current Two Factor status for an account with its user ID.
+     * @param userId {int} The numeric ID for the account.
+     * @returns {boolean} The status of the account having TFA.
+     */
     get_tfa_status(userId) {
-        const statement = this.db.prepare('SELECT tfa_protected FROM users WHERE id = ?');
-        return !!statement.get(userId);
+        const statement = this.db.prepare('SELECT tfa_enabled FROM users WHERE id = ?');
+        let val = statement.get(userId);
+        if(!val) return false;
+        return !!val.tfa_enabled;
     }
 
+    /**
+     * Checks if the user's password is valid.
+     * @param userId {int} The numeric ID for the account.
+     * @param password {String} The provided password for the account.
+     * @returns {boolean} The status of the password being valid.
+     */
     check_user_password(userId, password) {
         const statement = this.db.prepare('SELECT password FROM users WHERE id = ?');
         const val = statement.get(userId);
 
-        if(val === undefined) return false;
+        // Check that a value has been provided.
+        if(!val) return false;
 
-        // Hash the user password.
+        // Check the hash of the user password.
         return bcrypt.compare(password, val.password).then(result => {
             return result;
         });
